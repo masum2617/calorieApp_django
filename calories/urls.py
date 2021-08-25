1ed0c4ee983d142c75aab539ec72f7e1a51c3885
Masum Hasan <masum2617@gmail.com>
2021-08-25 18:43:45 +0600

    url edited

--- calories/urls.py
+++ calories/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     
-    # path('', views.index, name='calorie_index'),
+  
     path('', views.calories, name='calories'),
-    # path('calories/', views.calories, name='calories'),
+    path('calories/', views.calories, name='calories'),
    
 ]
